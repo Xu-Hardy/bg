598c82968961869d3d2dfd9213043e46d7b579a0
Xu <deep.work.max@gmail.com>
2023-10-27 08:44:39 +0800

    add api

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 
 
+def amazon_ip():
+    response = requests.get("https://checkip.amazonaws.com/")
+    # print(len(amzn))
+    print(response)
+    return response.text.strip()
+
+
+def speedtest():
+    response = requests.get("https://api-v3.speedtest.cn/ip")
+    return response.json()
+def ip_com():
+    response = requests.get("http://ip-api.com/json/")
+    return response.json()
+def ip_cn():
+    response = requests.get("https://ip.cn/api/index?type=0")
+    return response.json()
+
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello flask!'
+    ip = {'amzn': amazon_ip(), 'cn': ip_cn(),'speedtest':speedtest(),'ipcom':ip_com()}
+    # print(ip['cn'])
+
+    return ip
 
 
 @app.route('/a')
@@ -14,4 +35,4 @@ def xx():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True, port=9000)
